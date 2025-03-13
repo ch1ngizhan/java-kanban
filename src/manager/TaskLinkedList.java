2508c7d8e063c00b1cb5ffe85cfdbbbc11f72f86
Ch1ngizhan <egop.valuev@yandex.ru>
2025-03-13 18:37:01 +0300

    Перенес дир.test в manager

--- src/manager/TaskLinkedList.java
+++ src/manager/TaskLinkedList.java
@@ -5,39 +5,39 @@ import java.util.List;
 
 public class TaskLinkedList<T> {
 
-        public Node<T> head;
-        public Node<T> tail;
+    public Node<T> head;
+    public Node<T> tail;
 
 
-        TaskLinkedList() {
-            head = null;
-            tail = null;
-        }
+    TaskLinkedList() {
+        head = null;
+        tail = null;
+    }
 
-        Node<T> linkLast(T data) {
-            Node<T> newNode = new Node<>(data);
-            if (head == null) {
-                head = newNode;
-                tail = newNode;
+    Node<T> linkLast(T data) {
+        Node<T> newNode = new Node<>(data);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
 
-            } else {
-                tail.next = newNode;
-                newNode.prev = tail;
-                tail = newNode;
+        } else {
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
 
-
-            }
-            return newNode;
-        }
-
-        List<T> getTasks() {
-            Node<T> task = head;
-            List<T> result = new ArrayList<>();
-            while (task != null) {
-                result.add(task.data);
-                task = task.next;
-            }
-            return result;
 
         }
+        return newNode;
+    }
+
+    List<T> getTasks() {
+        Node<T> task = head;
+        List<T> result = new ArrayList<>();
+        while (task != null) {
+            result.add(task.data);
+            task = task.next;
+        }
+        return result;
+
+    }
 }
--- src/manager/test/InMemoryHistoryManagerTest.java
+++ src/manager/test/InMemoryHistoryManagerTest.java
@@ -1,4 +1,4 @@
-package test;
+package manager.test;
 
 import manager.HistoryManager;
 import manager.Managers;
