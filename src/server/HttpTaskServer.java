acbeef8b4a05a6995dc50187d5e7c731843c2788
ch1ngizhan <egop.valuev@yandex.ru>
2025-05-20 19:18:39 +0300

    Долполнил HttpTaskServer.

--- src/server/HttpTaskServer.java
+++ src/server/HttpTaskServer.java
@@ -9,16 +9,34 @@ import java.net.InetSocketAddress;
 
 public class HttpTaskServer {
 
-  TaskManager taskManager = Managers.getDefault();
+    private final TaskManager taskManager;
 
-
+public HttpTaskServer(TaskManager taskManager){
+    this.taskManager = taskManager;
+}
     public static void main(String[] args) throws IOException {
+        TaskManager taskManager = Managers.getDefault();
         HttpServer httpServer = HttpServer.create();
+        HttpTaskServer server = new HttpTaskServer(taskManager);
+        server.start(httpServer);
+    }
+
+
+    public void start(HttpServer httpServer) throws IOException {
 
         httpServer.bind(new InetSocketAddress(8080), 0);
+        httpServer.createContext("/tasks",new TaskHandler(taskManager));
+        httpServer.createContext("/subtasks",new SubtaskHandler(taskManager));
+        httpServer.createContext("/epics",new EpicHandler(taskManager));
+        httpServer.createContext("/history",new HistoryHandler(taskManager));
+        httpServer.createContext("/prioritized",new PrioritizedHandler(taskManager));
+        httpServer.start();
+    }
 
-            }
-        }
+    public void stop(HttpServer httpServer){
+        httpServer.stop(1);
+    }
+}
 
 
 
